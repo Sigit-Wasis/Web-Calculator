df16f556251cda77f595a8fae51cd1eee0a117a9
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-03-15 21:21:40 +0700

    add kalkulator.js

---
+++ assets/kalkulator.js
@@ -0,0 +1,34 @@
+const calculator = {
+   displayNumber: '0',
+   operator: null,
+   firstNumber: null,
+   waitingForSecondNumber: false
+};
+ 
+function updateDisplay() {
+   document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+ 
+function clearCalculator() {
+   calculator.displayNumber = '0';
+   calculator.operator = null;
+   calculator.firstNumber = null;
+   calculator.waitingForSecondNumber = false;
+}
+ 
+function inputDigit(digit) {
+   calculator.displayNumber += digit;
+}
+ 
+ 
+const buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+   button.addEventListener('click', function(event) {
+ 
+       // mendapatkan objek elemen yang diklik
+       const target = event.target;
+ 
+       inputDigit(target.innerText);
+       updateDisplay()
+   });
+}
